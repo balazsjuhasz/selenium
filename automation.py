8cc824e7abfcb961fcb15c9818ebcce6630f756a
Balazs Juhasz <juhaszbal@gmail.com>
2020-12-21 17:15:03 +0100

    first basic selenium commands

--- automation.py
+++ automation.py
@@ -2,6 +2,11 @@ from selenium import webdriver
 
 chrome_browser = webdriver.Chrome('./chromedriver')
 
-print(chrome_browser)
+chrome_browser.maximize_window()
+chrome_browser.get(
+    'https://www.seleniumeasy.com/test/basic-first-form-demo.html')
 
-chrome_browser
+
+assert 'Selenium Easy Demo' in chrome_browser.title
+button_text = chrome_browser.find_element_by_class_name('btn-default')
+print(button_text.get_attribute('innerHTML'))
